502fa5ef937970743cad1402dbba8364597a83bc
Jason Pereira <jason.pereira@actico.com>
2023-07-25 12:17:08 -0500

    changes

--- Employee Project/Employee_Project.md
+++ Employee Project/Employee_Project.md
@@ -43,8 +43,16 @@ For the options that require additional user input, ask for it.
 13. Edit Employee (user enters employee id)
 14. Most overworked employee (max number of projects assigned)
 15. Top 3 most overworked employees
-16. Compute Employee bonus. Bonus = 10 points + 5 points extra per project assigned. However, the 5 points are weighted by project priority. 0 points for a priority of 10 and 5 points for a priority of 1.
+16. Compute Employee bonus. Bonus = 10 points (base) + per project points (given in table)
 17. List employees by monthly salary (show name and monthly salary)
 
 Persist all data to disk using the pickle module. The data should be loaded from any pickle files if present.
 If no pickle files are present, load the initial data from the csv/json files
+
+
+
+| Priority  | Points |
+| ------------- | ------------- |
+| 1-3  | 5  |
+| 4-6  | 3  |
+| 7-10  | 2  |
---
+++ Employee Project/ep.py
@@ -0,0 +1,63 @@
+employees = []
+projects = []
+departments =[]
+
+def save_all_employees(emps):
+    pass
+
+def load_employees():
+    # check for the existence of pickle file/s
+    # If they exist, then load data from them, else from the csv file
+    pass
+
+def load_projects():
+    # load data from the csv file
+    pass
+
+def load_departments():
+    # load data from the csv file
+    pass
+
+def load_data():
+    load_employees()
+    load_projects()
+    load_departments()
+
+# add constructors for all the classes
+
+class Employee:
+    def compute_bonus(self):
+        pass
+    
+    def get_immediate_subordinates(self):
+        pass
+
+    def get_all_subordinates(self):
+        pass
+
+    def get_monthly_salary(self):
+        pass
+
+    def get_projects_assigned(self):
+        pass
+
+    def __str__(self):
+        # Override __str__() for all the classes
+        pass
+
+    
+
+class Project:
+    def __str__(self):
+        pass
+
+    def get_points(self):
+        pass
+
+class Department:
+    def __str__(self):
+        pass
+
+
+# main code goes here
+
